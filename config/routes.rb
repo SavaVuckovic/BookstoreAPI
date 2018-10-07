b3e6d5407f8d0ef819070d8cee75bb837fa69f68
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-10-07 04:26:42 +0200

    ai is correctly rendering JSON, started adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,10 @@
-
+Rails.application.routes.draw do
+  #categories
+  get '/categories', to: 'categories#index'
+  get '/categories/:id', to: 'categories#show'
+  # books
+  resources :books
+  root 'books#index'
+  # coments
+  # ...
+end
